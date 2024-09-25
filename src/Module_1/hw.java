27ba284f66d77ad4f993571e00c03cbea3e5b30a
samcr <samcralph@outlook.com>
2024-09-24 20:26:48 -0400

    First commit to repo

---
+++ src/Module_1/hw.java
@@ -0,0 +1,12 @@
+package Module_1;
+
+public class hw {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+        double c = (double) a /b;
+        System.out.println(c);
+        int d = a % b;
+        System.out.println(d);
+    }
+}
